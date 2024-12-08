17e96820b2a50e34768fbbd16ddd6880d3a7ab21
Extrafly1 <extrafly555367@gmail.com>
2024-12-08 12:19:59 +0300

    Add comments to the code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
+// Program starts here
 int main() {
+    // Declare variable for user's name
     std::string name;
     std::cout << "Enter your name: ";
     std::cin >> name;
