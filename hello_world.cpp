baa2fd91e136c603df561242c7945d083bd23267
Extrafly1 <extrafly555367@gmail.com>
2024-12-08 12:53:29 +0300

    Apply Mozilla style formatting

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
 // Program starts here
-int main() {
-    // Declare variable for user's name
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello, world from " << name << "!" << std::endl;
-    return 0;
+int
+main()
+{
+  // Declare variable for user's name
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello, world from " << name << "!" << std::endl;
+  return 0;
 }
